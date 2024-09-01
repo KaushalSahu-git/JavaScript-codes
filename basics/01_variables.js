84ed77102743078599dc1cc5e0a2bb938d77b1e0
Kaushlendra Sahu <163719553+KaushalSahu-git@users.noreply.github.com>
2024-09-01 21:27:28 +0000

    container config done

---
+++ basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "kaushal@google.com"
+var accountPassword = "12345"
+accountCity = "Noida"
+let accountState
+
+// accountId = 1 // not allowed
+accountEmail= "hacker@google.com"
+accountPassword="23456"
+accountCity="delhi"
+
+console.log(accountId);
+
+console.log([accountId, accountEmail, accountPassword, accountCity,accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
