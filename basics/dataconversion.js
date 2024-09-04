e811b0f12069e31c246a6ddc5f1296d912acaabb
Kaushlendra Sahu <163719553+KaushalSahu-git@users.noreply.github.com>
2024-09-04 20:43:44 +0000

    type conversion

---
+++ basics/dataconversion.js
@@ -0,0 +1,9 @@
+let score = "33" //null ,undefined, 0,true, "kaushal"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number(score) // boolean, string, number,
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
