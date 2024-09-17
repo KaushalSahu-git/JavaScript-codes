6d6f2e84abdd23d323c6161d43fa019ed48c08f6
Kaushlendra Sahu <163719553+KaushalSahu-git@users.noreply.github.com>
2024-09-17 19:32:02 +0000

    date

--- basics/07_date.js
+++ basics/07_date.js
@@ -6,3 +6,7 @@ console.log(myDate.toString())
 console.log(myDate.toDateString())
 console.log(myDate.toLocaleString())
 console.log(typeof myDate)
+
+let myCreatedDate = new Date(2023,0,14,5,3,40)
+console.log(myCreatedDate.toLocaleString())
+console.log(myCreatedDate.toJSON())
