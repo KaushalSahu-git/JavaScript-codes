f582ca5951094ebd5f660065315ad00e69e16804
Kaushlendra Sahu <163719553+KaushalSahu-git@users.noreply.github.com>
2024-09-17 19:12:03 +0000

    Math.random

--- basics/06_number_and_math.js
+++ basics/06_number_and_math.js
@@ -1,8 +1,23 @@
 const score = 500
-console.log(score)
+// console.log(score)
 
 const balance = new Number(100)
-console.log(balance)
+// console.log(balance)
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(3))
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(3))
+
+
+///////////////////////////////////////////////////////
+
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+// console.log(Math.floor((Math.random()*10))+1)
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random()*(max - min +1)+min))
+console.log(Math.floor(Math.random()*(max - min +1)+min))
+console.log(Math.floor(Math.random()*(max - min +1)+min))
+console.log(Math.floor(Math.random()*(max - min +1)+min))
+console.log(Math.floor(Math.random()*(max - min +1)+min))
