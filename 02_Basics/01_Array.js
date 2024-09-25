5ac334b5052e48810192c6a36f95a8f3faa63cdf
Kaushlendra Sahu <kaushal.git@gmail.com>
2024-09-25 19:16:08 +0000

    array

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,13 @@
+//Array
+
+const myarr = [0,1,2,3,4,5]
+const myheros = ["hacker","Developer"]
+
+const myarray = new Array(1,2,3,4,5)
+console.log(myarr[1]);
+
+//slice and splice 
+
+const myn1 = myarr.slice(1,3)
+console.log(myn1)
+console.log
